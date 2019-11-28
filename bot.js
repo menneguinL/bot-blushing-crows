09c1fc16a1ad6dfbb9abc319cfe7cbb54067b5c7
menneg_l <menneg_l@etna-alternance.net>
2019-11-28 14:49:29 -0500

    aezea

--- bot.js
+++ bot.js
@@ -62,8 +62,11 @@ bot.on('message', message => {
     		let array_role = message.content.split(' ');
     		i = 1
     		while (i < array_role.length) {
-    			if (array_role[i + 1].startsWith(':')) {
-    			message.reply(array_role[i] + " " + array_role[i + 1]);
+    			if (array_role[i + 1]) {
+
+    				if (array_role[i + 1].charAt(0) == ':' && array_role[i + 1].charAt(array_role[i + 1].length - 1) == ':') {
+    					message.reply(array_role[i] + " " + array_role[i + 1]);
+    				}
     			}
     			i = i + 2;
     		}
